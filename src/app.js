bb3d9989e5af27506e69362bc5ffedfe3e038d67
Jose Acosta <jdap.dmc@gmail.com>
2022-04-05 23:48:50 -0400

    feat: cors"

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./utils/routes.utils');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 router(app);
 
