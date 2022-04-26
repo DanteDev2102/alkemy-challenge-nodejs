858c66417b8f21706c110984c9e7e67fb4e504e9
Jose Acosta <jdap.dmc@gmail.com>
2022-04-26 07:28:32 -0400

    refactor: add middleware auth for routes

--- src/utils/routes.utils.js
+++ src/utils/routes.utils.js
@@ -3,10 +3,11 @@ const { join } = require('path');
 const characterRouter = require('../routes/character.routes');
 const userRouter = require('../routes/user.routes');
 const movieRouter = require('../routes/movie.routes');
+const { __auth } = require('../middlewares/auth.middleware');
 
 module.exports = (server) => {
 	server.use('/auth', userRouter);
-	server.use('/characters', characterRouter);
-	server.use('/movies', movieRouter);
-	server.use('/files', static(join(__dirname, '../files')));
+	server.use('/characters', __auth, characterRouter);
+	server.use('/movies', __auth, movieRouter);
+	server.use('/files', __auth, static(join(__dirname, '../files')));
 };
